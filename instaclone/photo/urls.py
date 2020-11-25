4cd6b7b55f2560843bb8ddc38b2ac738aeba7889
gabychoi <hojin.choi.dev@gmail.com>
2020-11-26 00:20:30 +0900

    instagram clone

--- instaclone/photo/urls.py
+++ instaclone/photo/urls.py
@@ -10,3 +10,7 @@ urlpatterns = [
     path("", PhotoList.as_view(), name='index'),
 ]
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
